e43f6f71f6f7f18bacff893ac5a9eea36bfeacac
pat-s <patrick.schratz@gmail.com>
2019-11-10 00:57:23 +0100

    try fixing setup_push_deploy

--- tic.R
+++ tic.R
@@ -5,6 +5,6 @@ if (ci_on_travis()) {
   get_stage("deploy") %>%
     add_code_step(rmarkdown::render("README.Rmd")) %>%
     add_step(step_setup_ssh()) %>%
-    add_step(step_setup_push_deploy()) %>%
+    add_step(step_setup_push_deploy(".", branch = "master", orphan = FALSE)) %>%
     add_step(step_push_deploy(commit_paths = c("README.md", "README_files/")))
 }
